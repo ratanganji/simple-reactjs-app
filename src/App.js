8de3ff2abcfa35764ca43fb91f3a2f69fab75608
CodeByCreative <rathankumar2024@gmail.com>
2026-04-26 20:30:03 +0530

    UI changed for practice

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simple React App</h1>
         </header>
+        console.log("practicing git");
           <Switch>
                 <Route exact path= "/" render={() => (
                   <Redirect to="/customerlist"/>
